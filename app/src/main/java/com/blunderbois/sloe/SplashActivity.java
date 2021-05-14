47ff5667a68f930e16dfab2d5f13e94889c6ec9e
Arjun <arjun140702@gmail.com>
2021-05-14 17:31:09 +0530

    Modified dependencies, Added Firebase

--- app/src/main/java/com/blunderbois/sloe/SplashActivity.java
+++ app/src/main/java/com/blunderbois/sloe/SplashActivity.java
@@ -1,7 +1,7 @@
 package com.blunderbois.sloe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
