14687d24124807cdd10b1e447e4ee20d82a266f7
Prasoon Soni <prasoonsoni03@gmail.com>
2021-05-14 18:17:18 +0530

    Added Login Activity

--- app/src/main/java/com/blunderbois/sloe/MainActivity.java
+++ app/src/main/java/com/blunderbois/sloe/MainActivity.java
@@ -1,13 +1,28 @@
 package com.blunderbois.sloe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mAuth.getCurrentUser()==null){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
     }
 }
